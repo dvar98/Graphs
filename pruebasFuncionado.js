697affb16a8fc61b7ccc02474626b4b8357ab192
dvar98 <varela13guerrero@gmail.com>
2024-02-12 17:19:18 -0500

    Update pruebasFuncionado.js

--- pruebasFuncionado.js
+++ pruebasFuncionado.js
@@ -463,7 +463,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const numNodes = nodes.length;
 
-    let matriz = new Array(numNodes).fill(null).map(() => new Array(numNodes).fill(0));
+    let matriz = new Array(numNodes).fill(null).map(() => new Array(numNodes).fill(75));
 
     links.forEach(link => {
       let source = link.source.id;
